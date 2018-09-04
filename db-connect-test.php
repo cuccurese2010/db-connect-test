c4b514b924c59465153cb0f4d6ab1eeb8a20242d
Raffaele Cuccurese <41526787+cuccurese2010@users.noreply.github.com>
2018-09-04 11:12:17 -0500

    Update db-connect-test.php

--- db-connect-test.php
+++ db-connect-test.php
@@ -1,10 +1,10 @@
 <?php
 # Fill our vars and run on cli
 # $ php -f db-connect-test.php
-$dbname = 'name';
-$dbuser = 'user';
-$dbpass = 'pass';
-$dbhost = 'host';
+$dbname = 'lllc';
+$dbuser = 'root';
+$dbpass = 'root';
+$dbhost = 'mysql';
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
